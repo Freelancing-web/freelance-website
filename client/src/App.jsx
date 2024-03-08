7ece930f9ac507ca000f03a520dd4e650f181e01
unknown <Caamir Muse Hassan>
2024-03-08 21:25:07 +0300

    create something

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="flex justify-center items-center text-4xl h-screen">
     <h1>Welcome to Freelancing Website</h1>
+    <h1>Where Talent People and business needs meets</h1>
   </div>
   )
 }
