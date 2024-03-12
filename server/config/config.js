8e64bee7265dee0a843abddda5a945171b108473
abdikarim-dev <cabdikariim405@gmail.com>
2024-03-12 21:52:30 +0300

    Configuration Added

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config();
+export const MONGO_URL = process.env.MONGODB_URL;
+export const PORT = process.env.PORT;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY  = process.env.CLOUDINARY_API_KEY ;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
